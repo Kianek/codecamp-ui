969e3d18b8a3efd9216adf27c6903a1cead259ea
Kianek <kallridge92@gmail.com>
2020-07-09 08:31:16 -0700

    Add base routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { StudentProgressComponent } from './pages/student-progress/student-progress.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'user/:name',
+    component: UserDetailComponent,
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent,
+  },
+  {
+    path: 'student-progress',
+    component: StudentProgressComponent,
+  },
+  {
+    path: '*',
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
