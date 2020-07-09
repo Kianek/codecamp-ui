0721d5ab816b9115132cf6fb61b705128ce2d6c4
Kianek <kallridge92@gmail.com>
2020-07-09 08:24:54 -0700

    Add page/layout components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { StudentProgressComponent } from './pages/student-progress/student-progress.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SettingsComponent,
+    DashboardComponent,
+    StudentProgressComponent,
+    UserDetailComponent,
+    NotFoundComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
